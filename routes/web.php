c1b6710171eac8e21fc8fbae065d9972f12e23fc
Camilo Jaramillo <jaracamilo@gmail.com>
2025-03-11 11:06:27 -0500

    ♻️ Actualización de las rutas
    Se adiciona la ruta para registrar una cuenta

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('principal');
 });
+
+Route::get('/crear-cuenta',[RegisterController::class,'index'])->name('register');
+Route::post('/crear-cuenta',[RegisterController::class,'store'])->name('register');
